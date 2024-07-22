bb5da5736fdbec411cb30f16161f7fb31f769380
minnong511 <kzxx1234@seoultech.ac.kr>
2024-07-23 06:18:56 +0900

    집에서 07.23

--- minnong/mini_batch_two_layer_net.py
+++ minnong/mini_batch_two_layer_net.py
@@ -10,10 +10,12 @@ train_size = x_train.shape[0] # 60000개
 batch_size = 100 # 샘플로 뽑아낼 데이터 갯수
 learning_rate = 0.1 # 학습률
 
+# Layer 갯수 초기화  :  input = image(width * depth) , hidden = 은닉층 , output size
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 for i in range(iters_num):
     # 미니배치 획득
+    # 무작위로 100개를 뽑는다.
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -28,3 +30,5 @@ for i in range(iters_num):
     # 학습 경과 기록
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+# backpropagation 을 알고 강의를 들어서 그런가.. 더 헷갈리네...
